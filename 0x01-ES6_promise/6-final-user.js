4a9e256923f47e01d5ab0df05cfd4af672a26b82
Joe <joeegboka@gmail.com>
2024-04-24 23:11:18 +0100

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(
+    (values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === "rejected") {
+          res.push({ status: value.status, value: value.reason });
+        } else {
+          res.push({ status: value.status, value: value.value });
+        }
+      });
+      return res;
+    }
+  );
+}
+
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
