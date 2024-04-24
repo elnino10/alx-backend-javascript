21cd66ca28c393ea75ed71bcc5db4c23c882abc1
Joe <joeegboka@gmail.com>
2024-04-25 00:47:05 +0100

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const val = mathFunction();
+    queue.push(val);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push("Guardrail was processed");
+  }
+  return queue;
+}
