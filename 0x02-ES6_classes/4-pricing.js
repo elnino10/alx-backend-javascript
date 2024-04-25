acd22a858a2f2f3c77fc357c8c4b739ed1aaf21c
Joe <joeegboka@gmail.com>
2024-04-26 00:00:44 +0100

    5-building.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,40 +1,30 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number')
-      throw new TypeError('Amount must be a number');
-    if (!(currency instanceof Currency))
-      throw new TypeError('Currency must be a Currency');
     this._amount = amount;
     this._currency = currency;
-  }
-
-  set amount(amount) {
-    if (typeof amount === 'number') {
-      this._amount = amount;
-    } else {
-      throw new TypeError('Amount must be a number');
-    }
-  }
-
-  set currency(currency) {
-    if (currency instanceof Currency) {
-      this._currency = currency;
-    } else {
-      throw new TypeError('Currency must be a Currency');
-    }
   }
 
   get amount() {
     return this._amount;
   }
 
+  set amount(a) {
+    this._amount = a;
+  }
+
   get currency() {
     return this._currency;
   }
 
+  set currency(c) {
+    this._currency = c;
+  }
+
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
   }
 }
