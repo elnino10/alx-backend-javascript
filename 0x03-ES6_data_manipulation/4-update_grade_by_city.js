f1ff6b3e6712fb5b0b5a0cd44846ec09c925a63c
Joe <joeegboka@gmail.com>
2024-04-30 22:39:54 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,13 +3,13 @@ export default function updateStudentGradeByCity(studentsArray, city, newGrade) 
   return studentsArray
     .filter((student) => student.location === city)
     .map((student) => {
-        const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
-        if (gradeObj === undefined) {
-            return {...student, grade: 'N/A'};
-        }
-        return {
-            ...student,
-            grade: gradeObj.grade || 'N/A'
-        };
+      const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
+      if (gradeObj === undefined) {
+        return { ...student, grade: 'N/A' };
+      }
+      return {
+        ...student,
+        grade: gradeObj.grade || 'N/A',
+      };
     });
 }
